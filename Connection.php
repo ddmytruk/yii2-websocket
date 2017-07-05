26338ede456cffe366f70796ffd877df625c5505
Dmytro Dmytruk <d.dmytruk@icloud.com>
2017-07-05 14:01:35 +0300

    base

---
+++ Connection.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmytrodmytruk
+ * Date: 05.07.17
+ * Time: 14:00
+ */
+
+namespace ddmytruk\websocket;
+
+use yii\base\Component;
+
+class Connection extends Component {
+
+}
